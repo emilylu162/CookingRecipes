902f51e272d901b18d81dd879897e829783462ec
Emily Lu <emilylu162@g.ucla.edu>
2025-04-17 15:48:38 -0700

    image functionality and edit existing recipes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# CookingRecipes
+# Stirring Stories
+This is a work-in-progress Golang web app. The goal is to create a full-stack app integrated with SQL to dynamically update data (recipes) as well as full user functionality.
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/gorilla/mux"
@@ -14,10 +17,11 @@ type Recipe struct {
 	Title       string
 	Description string
 	Time        string
+	ImagePath   string
 }
 
 var recipes = []Recipe{
-	{ID: 1, Title: "Shin Ramen", Description: "Your favorite ramen...but upgraded", Time: "10 minutes"},
+	{ID: 1, Title: "Shin Ramen", Description: "Your favorite ramen...but upgraded", Time: "10 minutes", ImagePath: "./uploads/images/shin.jpg"},
 	{ID: 2, Title: "Shrimp Dumplings", Description: "Classic dumplings without the carbs", Time: "15 minutes"},
 }
 
@@ -25,12 +29,16 @@ func main() {
 	r := mux.NewRouter()
 
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	r.PathPrefix("/uploads/").Handler(http.StripPrefix("/uploads/", http.FileServer(http.Dir("uploads"))))
 
 	r.HandleFunc("/", homeHandler)
 	r.HandleFunc("/recipes", listRecipesHandler)
 
 	r.HandleFunc("/recipes/new", newRecipeFormHandler).Methods("GET")
 	r.HandleFunc("/recipes/new", createRecipeHandler).Methods("POST")
+
+	r.HandleFunc("/recipes/{id}/edit", editRecipeFormHandler).Methods("GET")
+	r.HandleFunc("/recipes/{id}/edit", updateRecipeHandler).Methods("POST")
 
 	r.HandleFunc("/recipes/{id}", showRecipeHandler)
 
@@ -78,24 +86,96 @@ func newRecipeFormHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func createRecipeHandler(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
+	err := r.ParseMultipartForm(10 << 20) // 10 MB max memory
 	if err != nil {
-		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		http.Error(w, "Could not parse form", http.StatusBadRequest)
 		return
 	}
 
-	// Create a new Recipe from form values
-	newID := len(recipes) + 1
-	newRecipe := Recipe{
-		ID:          newID,
-		Title:       r.FormValue("title"),
-		Description: r.FormValue("description"),
-		Time:        r.FormValue("time"),
+	title := r.FormValue("title")
+	time := r.FormValue("time")
+	description := r.FormValue("description")
+	imagePath := ""
+
+	// Handle image upload
+	file, handler, err := r.FormFile("image")
+	if err == nil {
+		defer file.Close()
+		fileName := fmt.Sprintf("%d_%s", len(recipes)+1, handler.Filename)
+		imagePath = "uploads/images/" + fileName
+
+		dst, err := os.Create(imagePath)
+		if err != nil {
+			http.Error(w, "Could not save image", http.StatusInternalServerError)
+			return
+		}
+		defer dst.Close()
+		io.Copy(dst, file)
 	}
 
-	// Add to the in-memory slice
+	newRecipe := Recipe{
+		ID:          len(recipes) + 1,
+		Title:       title,
+		Time:        time,
+		Description: description,
+		ImagePath:   "/" + imagePath, // prep for web path
+	}
+
 	recipes = append(recipes, newRecipe)
 
-	// Redirect to /recipes
 	http.Redirect(w, r, "/recipes", http.StatusSeeOther)
+}
+
+func editRecipeFormHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(idStr)
+
+	for _, recipe := range recipes {
+		if recipe.ID == id {
+			renderTemplate(w, "recipe_edit", recipe)
+			return
+		}
+	}
+	http.NotFound(w, r)
+}
+
+func updateRecipeHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(idStr)
+
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		http.Error(w, "Could not parse form", http.StatusBadRequest)
+		return
+	}
+
+	for i := range recipes {
+		if recipes[i].ID == id {
+			recipes[i].Title = r.FormValue("title")
+			recipes[i].Time = r.FormValue("time")
+			recipes[i].Description = r.FormValue("description")
+
+			// Handle optional new image
+			file, handler, err := r.FormFile("image")
+			if err == nil {
+				defer file.Close()
+				fileName := fmt.Sprintf("%d_%s", id, handler.Filename)
+				imagePath := "uploads/images/" + fileName
+
+				dst, err := os.Create(imagePath)
+				if err != nil {
+					http.Error(w, "Could not save image", http.StatusInternalServerError)
+					return
+				}
+				defer dst.Close()
+				io.Copy(dst, file)
+
+				recipes[i].ImagePath = "/" + imagePath
+			}
+
+			break
+		}
+	}
+
+	http.Redirect(w, r, "/recipes/"+idStr, http.StatusSeeOther)
 }
